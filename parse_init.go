578e2bd923cf72dd866d76faf671ec468ced318c
HunterYuan <hunter_yuan@outlook.com>
2018-05-05 10:49:20 +0800

    书写序列化和反序列化的函数实现流程说明

--- parse_init.go
+++ parse_init.go
@@ -16,20 +16,21 @@ func parseSection(line string, typeInfo reflect.Type) (fieldName string, err err
 		err = fmt.Errorf("syntax error, invalid section %s", line)
 	}
 
-	sectionName := strings.TrimSpace(line[1 : len(line)-1])
-	if 0 == len(fieldName) {
-		err = fmt.Errorf("syntax error, invalid section %s", line)
-		return
-	}
+	if line[0] == '[' && line[len(line)-1] == ']' {
+		sectionName := strings.TrimSpace(line[1 : len(line)-1])
+		if 0 == len(fieldName) {
+			err = fmt.Errorf("syntax error, invalid section %s", line)
+			return
+		}
 
-	for i := 0; i < typeInfo.NumField(); i++ {
-		filed := typeInfo.Field(i)
-		tagValue := filed.Tag.Get("init")
-		if tagValue == sectionName {
-			fieldName = filed.Name
-			break
+		for i := 0; i < typeInfo.NumField(); i++ {
+			filed := typeInfo.Field(i)
+			tagValue := filed.Tag.Get("init")
+			if tagValue == sectionName {
+				fieldName = filed.Name
+				break
+			}
 		}
 	}
-
 	return
 }
